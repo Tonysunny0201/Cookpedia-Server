6af80e80012652c0c6a25fd8324eb0a38313ea49
Tony <tony603t@gmail.com>
2024-12-05 16:20:31 +0530

    cookpedia-server

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const users = require('../models/userModel')
+const bcrypt = require('bcrypt') 
+const jwt = require('jsonwebtoken')
+
+
+// add user
+exports.addUserController = async (req,res)=>{
+
+    console.log("Inside addUserController");
+    const {username,email,password} = req.body
+    try {
+        const exisitingUser = await users.findOne({email})
+        if(exisitingUser){
+            res.status(406).json("User Already Exisit... please login")
+        }else{
+            const encryptedPassword = await bcrypt.hash(password,10)
+            const newUser = new users({
+                username,email,password:encryptedPassword
+            })
+            await newUser.save()
+            res.status(200).json(newUser)
+        }
+
+    } catch (err) {
+        res.status(401).json(err)
+    }
+}
+
+// login
+exports.loginController = async (req,res)=>{
+    console.log("Inside addUserController");
+    const {email,password} = req.body
+    try {
+        const exisitingEmail = await users.findOne({email})
+        if(exisitingEmail){
+            let isUserPaswdMatch = await bcrypt.compare(password,exisitingEmail.password)
+            if(isUserPaswdMatch || password==exisitingEmail.password){
+                const token = jwt.sign({userId:exisitingEmail._id},process.env.JWTPASSWORD)
+                res.status(200).json({user:exisitingEmail,token})
+            }else{
+                res.status(404).json("Invalid email / password")
+            }
+        }else{
+            res.status(404).json("Invalid email / password")
+        }
+    } catch (err) {
+        res.status(401).json(err)
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const recipeController = require('../controllers/recipeController')
 const testimonyController = require('../controllers/testimonyController')
+const userController = require('../controllers/userController')
+
 const router = new express.Router()
 
 // all recipes
@@ -8,4 +10,12 @@ router.get("/all-recipes",recipeController.getAllRecipeController)
 
 // add testimony
 router.post("/add-testimony",testimonyController.addTestimonyController)
+
+// add user  : register
+router.post("/register",userController.addUserController)
+
+// login
+router.post("/login",userController.loginController)
+
 module.exports = router
+
